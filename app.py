b3ce9953aa43b8f32642532ca9f17c9147788163
unknown <belloafeez28@gmail.com>
2024-10-04 23:11:58 +0100

    added new my id

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ from firebase_admin import credentials
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
-
-load_dotenv()
 from urls import badify
 
-secret_key = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-cred = credentials.Certificate('secret_key')
+cred = credentials.Certificate('securityAccountKey.json')
 firebase_admin.initialize_app(cred)
 app.register_blueprint(badify)
 
